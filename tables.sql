6bc74f1e0ef8f076d8e79031a3cdb26fc3bc1225
PaoCuellar <paola.cuellar@mail.escuelaing.edu.co>
2020-04-29 22:51:31 -0500

    Create Iniciative mapper

--- tables.sql
+++ tables.sql
@@ -5,6 +5,25 @@ CREATE TABLE IF NOT EXISTS USUARIO (
   tipoUsuario VARCHAR(20) NOT NULL,
   email VARCHAR(150) UNIQUE);
 
+CREATE TABLE IF NOT EXISTS COMENTARIO(
+  id  INT(20) PRIMARY KEY,
+  fecha DATE(20) NOT NULL,
+  autor VARCHAR(20) NOT NULL,
+  contenido VARCHAR(20) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS INICIATIVA (
+  descripcion VARCHAR(50) NOT NULL,
+  votos INT(20) NOT NULL,
+  fecha DATE(20) NOT NULL,
+  nombre_proponente VARCHAR(20) NOT NULL,
+  areas VARCHAR(150) NOT NULL,
+  dependencia VARCHAR(20) NOT NULL,
+  palabras_clave VARCHAR(20) NOT NULL,
+  comentario VARCHAR(20), 
+  FOREIGN KEY (comentario) REFERENCES COMENTARIO (id));
+
+
+     
 
 insert into usuario (nombre,documento,contrasena,tipoUsuario,email) values ('Paola','2106888','1234','Administrador','paola.cuellar@mail.escuelaing.edu.co');
 insert into usuario (nombre,documento,contrasena,tipoUsuario,email) values ('David','2107329','admin','Administrador','david.otalora@mail.escuelaing.edu.co');
