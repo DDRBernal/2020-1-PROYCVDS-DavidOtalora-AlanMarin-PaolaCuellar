6463e0827e4fd7fce2f23d9caea8f3a7993e9f01
PaoCuellar <paola.cuellar@mail.escuelaing.edu.co>
2020-05-15 22:53:32 -0500

    fix test

---
+++ src/test/java/edu/eci/cvds/TestServicesIniciativa.java
@@ -0,0 +1,66 @@
+package edu.eci.cvds;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.Assert;
+
+import edu.eci.cvds.entities.Usuario;
+import edu.eci.cvds.entities.Iniciativa;
+import edu.eci.cvds.services.ServicesBancoProyectoFactory;
+import edu.eci.cvds.services.ServicesIniciativa;
+import edu.eci.cvds.services.ServicesUsuario;
+public class TestServicesIniciativa {
+    
+    @Inject
+    private SqlSession sqlSession;
+
+    ServicesUsuario servicesUsuario;
+    ServicesIniciativa servicesIniciativa;
+    
+    public TestServicesIniciativa() {
+        servicesIniciativa = ServicesBancoProyectoFactory.getInstance().getServicesIniciativa();
+        
+    }
+
+    /**
+    @Test
+    public void insertarIniciativa(){
+        servicesIniciativa.crearIniciativa("Glass JDM", new SimpleDateFormat("dd/mm/yyyy").format(new Date(15/05/2020)), "revision", "proponente2", "industrial","industrial", "Es un mecanismo para todas las propiedades horizontales, que puedan limpiar los vidrios en las alturas instalando una única vez el producto");
+    }
+    */
+
+    
+    @Test
+    public void consultarIniciativa(){
+        try{
+            servicesIniciativa.consultarIniciativa("Protesis de brazos");
+        }catch(Exception e){
+            Assert.assertTrue(false);
+        }
+    }
+    
+    @Test
+    public void consultarPorArea(){
+        try{
+            servicesIniciativa.consultarPorArea("industrial");
+        }catch(Exception e){
+            Assert.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void consultarPorEstado(){
+        try{
+            servicesIniciativa.consultarPorEstado("revision");
+        }catch(Exception e){
+            Assert.assertTrue(false);
+        }
+    }
+}
---
+++ src/test/java/edu/eci/cvds/TestServicesUsuario.java
@@ -0,0 +1,56 @@
+package edu.eci.cvds;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.Assert;
+
+import edu.eci.cvds.entities.Usuario;
+import edu.eci.cvds.services.ServicesBancoProyectoFactory;
+import edu.eci.cvds.services.ServicesUsuario;
+
+public class TestServicesUsuario {
+    
+    
+    @Inject
+    private SqlSession sqlSession;
+
+    ServicesUsuario servicesUsuario;
+
+    public TestServicesUsuario(){
+        servicesUsuario = ServicesBancoProyectoFactory.getInstance().getServicesUsuario();    
+    }
+
+    @Test
+    public void consultarUsuario() {
+    	try {
+    		servicesUsuario.consultarUsuario("admin");
+    	}catch(Exception e){
+    		Assert.assertTrue(false);    		
+    	}  	
+    }
+
+    @Test
+    public void consultarUsuarios() {
+    	try {
+    		servicesUsuario.consultarUsuariosPublico("pmo");
+    	}catch(Exception e){
+    		Assert.assertTrue(false);    		
+    	}  	
+    }
+
+    @Test
+    public void insertarUsuario(){
+        servicesUsuario.insertarUsuario(new Usuario("JuanMe",1019223112,"Juan","Mendez","estudiante","ju4nm3","pmo","juanme@mail.escuelaing.edu.co"));
+        try{
+            servicesUsuario.consultarUsuario("JuanMe");
+        }catch(Exception e){
+            Assert.assertTrue(false);
+        }
+    }
+
+}
